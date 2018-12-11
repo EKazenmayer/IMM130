1125c5f100d7c2232d97935cf78e8fa4363c6569
Emily Kazenmayer <kazenme1@tcnj.edu>
2018-12-11 16:30:08 -0500

    finalizing tooltips, styles, etc

--- scripts/main.js
+++ scripts/main.js
@@ -33,7 +33,7 @@ function submitLesson1() {
         window.alert("Please provide valid input for all fields.");
     } else {
         cookies.set("name", encodeHTML(name));
-        cookies.set("age", encodeHTML(age));
+        cookies.set("age", encodeHTML(parseInt(age, 10).toString()));
 
         window.location.href = "lesson1a.html";
     }
@@ -42,21 +42,23 @@ function submitLesson1() {
 function loadLesson1a() {
     let name = cookies.get("name");
     let age = cookies.get("age");
-
-    document.getElementById("lesson1a0").innerHTML = "Welcome to the Cafe, " + name;
-    document.getElementById("lesson1a1").innerHTML = "You're " + age + "? Cool beans! In fact, if today was your birthday, the cafe would give you " + age + " coffee beans - one for each year!";
+    if (name != null && age != null) {
+        document.getElementById("lesson1a0").innerHTML = "Welcome to the Cafe, " + name;
+        document.getElementById("lesson1a1").innerHTML = "You're " + age + "? Cool beans! In fact, if today was your birthday, the cafe would give you " + age + " coffee beans - one for each year!";
+    }
 }
 
 function loadLesson1b() {
     let name = cookies.get("name");
     let age = cookies.get("age");
-
-    document.getElementById("lesson1code").innerHTML =
-        "String name = \"" + name + "\";\n" +
-        "int age = " + age + ";\n" +
-        "System.out.println(\"Welcome to the Cafe, \" + name + \"!\");\n" +
-        "System.out.print(\"You're \" + age + \"? Cool beans! \");\n" +
-        "System.out.println(\"In fact, if today was your birthday, we would give you \" + age + \" coffee beans - one for each year!\");\n";
+    if (name != null && age != null) {
+        document.getElementById("lesson1code0").innerHTML = name;
+        document.getElementById("lesson1code1").innerHTML = age;
+    }
+    //        "int age = " + age + ";\n" +
+    //        "System.out.println(\"Welcome to the Cafe, \" + name + \"!\");\n" +
+    //        "System.out.print(\"You're \" + age + \"? Cool beans! \");\n" +
+    //        "System.out.println(\"In fact, if today was your birthday, we would give you \" + age + \" coffee beans - one for each year!\");\n";
 }
 
 function submitLesson2() {
@@ -67,7 +69,7 @@ function submitLesson2() {
         window.alert("Please provide valid input for all fields.");
     } else {
         cookies.set("drinksCoffee", drinksCoffee);
-        cookies.set("numPackets", encodeHTML(numPackets));
+        cookies.set("numPackets", encodeHTML(parseInt(numPackets, 10).toString()));
 
         window.location.href = "lesson2a.html";
     }
@@ -76,49 +78,50 @@ function submitLesson2() {
 function loadLesson2a() {
     let drinksCoffee = cookies.get("drinksCoffee");
     let numPackets = cookies.get("numPackets");
+    if (drinksCoffee != null && numPackets != null) {
+        if (drinksCoffee == "true") {
+            typeOfDrink = "coffee";
+        } else {
+            typeOfDrink = "tea";
+        }
 
-    if (drinksCoffee == "true") {
-        typeOfDrink = "coffee";
-    } else {
-        typeOfDrink = "tea";
+        if (numPackets == 0 || numPackets == 1) {
+            customMessage = "It's really bitter!";
+        } else if (numPackets == 2) {
+            customMessage = "Just right!";
+        } else if (numPackets >= 3) {
+            customMessage = "How sweet!";
+        }
+
+        document.getElementById("lesson2a").innerHTML = "She made you " + typeOfDrink + " with " + numPackets + " packets of sugar.<br>" + customMessage;
     }
-
-    if (numPackets == 0) {
-        customMessage = "It's really bitter!";
-    } else if (numPackets == 1 || numPackets == 2) {
-        customMessage = "Just right!";
-    } else if (numPackets >= 3) {
-        customMessage = "How sweet!";
-    }
-
-    document.getElementById("lesson2a").innerHTML = "She made you " + typeOfDrink + " with " + numPackets + " packets of sugar.<br>" + customMessage;
 }
 
 function loadLesson2b() {
     let drinksCoffee = cookies.get("drinksCoffee");
     let numPackets = cookies.get("numPackets");
-
-    document.getElementById("lesson2code").innerHTML =
-        "boolean drinksCoffee = " + drinksCoffee + ";\n" +
-        "int numPackets = " + numPackets + ";\n" +
-        "String typeOfDrink;\nif (drinksCoffee == true) {\n" +
-        "    typeOfDrink = \"coffee\";\n" +
-        "}\n" +
-        "else {\n" +
-        "    typeOfDrink = \"tea\";\n" +
-        "}\n" +
-        "System.out.print(\"Barista Ada made you a special drink! \");\n" +
-        "System.out.println(\"She made you \" + typeOfDrink + \" with \" + numPackets + \" packets of sugar.\");\n" +
-        "\n" +
-        "if (numPackets == 0) {\n" +
-        "    System.out.println(\"It's really bitter!\");\n" +
-        "}\n" +
-        "else if (numPackets == 1 || numPackets == 2) {\n" +
-        "    System.out.println(\"Just right!\");\n" +
-        "}\n" +
-        "else if (numPackets >= 3) {\n" +
-        "    System.out.println(\"How sweet!\");\n" +
-        "}";
+    if (drinksCoffee != null && numPackets != null) {
+        document.getElementById("lesson2code0").innerHTML = drinksCoffee;
+        document.getElementById("lesson2code0").innerHTML = numPackets;
+        //            "String typeOfDrink;\nif (drinksCoffee == true) {\n" +
+        //            "    typeOfDrink = \"coffee\";\n" +
+        //            "}\n" +
+        //            "else {\n" +
+        //            "    typeOfDrink = \"tea\";\n" +
+        //            "}\n" +
+        //            "System.out.print(\"Barista Ada made you a special drink! \");\n" +
+        //            "System.out.println(\"She made you \" + typeOfDrink + \" with \" + numPackets + \" packets of sugar.\");\n" +
+        //            "\n" +
+        //            "if (numPackets == 0 || numPackets == 1) {\n" +
+        //            "    System.out.println(\"It's really bitter!\");\n" +
+        //            "}\n" +
+        //            "else if (numPackets == 2) {\n" +
+        //            "    System.out.println(\"Just right!\");\n" +
+        //            "}\n" +
+        //            "else {\n" +
+        //            "    System.out.println(\"How sweet!\");\n" +
+        //            "}";
+    }
 }
 
 function submitLesson3() {
@@ -146,8 +149,9 @@ function loadLesson3a() {
         let id = customer + i;
         customers[i] = cookies.get(id);
     }
-
-    document.getElementById("lesson3a").innerHTML = "First, please take " + customers[0] + "\'s order. Next, serve " + customers[1] + ", then get to " + customers[2] + ", followed by " + customers[3] + ". Finally, please finish the order for " + customers[4] + ". Good luck! You're doing great!";
+    if (customers[0] != null) {
+        document.getElementById("lesson3a").innerHTML = "First, please take " + customers[0] + "\'s order. Next, serve " + customers[1] + ", then get to " + customers[2] + ", followed by " + customers[3] + ". Finally, please finish the order for " + customers[4] + ". Good luck! You're doing great!";
+    }
 }
 
 function loadLesson3b() {
@@ -157,21 +161,24 @@ function loadLesson3b() {
         let id = customer + i;
         customers[i] = cookies.get(id);
     }
-
-    document.getElementById("lesson3code").innerHTML =
-        "String[] customers = {\n" +
-        "    \"" + customers[0] + "\",\n" +
-        "    \"" + customers[1] + "\",\n" +
-        "    \"" + customers[2] + "\",\n" +
-        "    \"" + customers[3] + "\",\n" +
-        "    \"" + customers[4] + "\"\n" +
-        "};\n" +
-        "System.out.println(\"Barista Bill has orders for you to fill!\");\n" +
-        "System.out.println(\"First, please take an order for \" + customers[0] + \n" +
-        "    \". Next, serve \" + customers[1] + \", then get to \" + customers[2] + \n" +
-        "    \", followed by \" + customers[3] + \n" +
-        "    \". Finally, please finish the order for \" + customers[4] + \n" +
-        "    \". Good luck! You're doing great!\");";
+    if (customers[0] != null) {
+        document.getElementById("lesson3code0").innerHTML = "\"" + customers[0] + "\",";
+        document.getElementById("lesson3code0").innerHTML = "\"" + customers[1] + "\",";
+        document.getElementById("lesson3code0").innerHTML = "\"" + customers[2] + "\",";
+        document.getElementById("lesson3code0").innerHTML = "\"" + customers[3] + "\",";
+        document.getElementById("lesson3code0").innerHTML = "\"" + customers[4] + "\"";
+        //            "    \"" + customers[1] + "\",\n" +
+        //            "    \"" + customers[2] + "\",\n" +
+        //            "    \"" + customers[3] + "\",\n" +
+        //            "    \"" + customers[4] + "\"\n" +
+        //            "};\n" +
+        //            "System.out.println(\"Barista Bill has orders for you to fill!\");\n" +
+        //            "System.out.println(\"First, please take an order for \" + customers[0] + \n" +
+        //            "    \". Next, serve \" + customers[1] + \", then get to \" + customers[2] + \n" +
+        //            "    \", followed by \" + customers[3] + \n" +
+        //            "    \". Finally, please finish the order for \" + customers[4] + \n" +
+        //            "    \". Good luck! You're doing great!\");";
+    }
 }
 
 function submitLesson4() {
@@ -179,7 +186,7 @@ function submitLesson4() {
     if (amountMoreCups == null || amountMoreCups == "" || parseInt(amountMoreCups, 10) < 0 || parseInt(amountMoreCups, 10) > 536870911) {
         window.alert("Please provide valid input for all fields.");
     } else {
-        cookies.set("amountMoreCups", encodeHTML(amountMoreCups));
+        cookies.set("amountMoreCups", encodeHTML(parseInt(amountMoreCups, 10).toString()));
 
         window.location.href = "lesson4a.html";
     }
@@ -189,23 +196,27 @@ function loadLesson4a() {
     let lesson = "lesson4a";
     let amount = 2;
     let amountMoreCups = cookies.get("amountMoreCups");
-    for (let i = 0; i < 5; i++) {
-        let id = lesson + i;
-        document.getElementById(id).innerHTML = "Customer #" + (i + 1) + " ordered " + amount + " cups of coffee."
-        amount += parseInt(amountMoreCups, 10);
+    if (amountMoreCups != null) {
+        for (let i = 0; i < 5; i++) {
+            let id = lesson + i;
+            document.getElementById(id).innerHTML = "Customer #" + (i + 1) + " ordered " + amount + " cups of coffee."
+            amount += amountMoreCups;
+        }
     }
 }
 
 function loadLesson4b() {
     let amountMoreCups = cookies.get("amountMoreCups");
-    document.getElementById("lesson4code").innerHTML =
-        "int amountMoreCups = " + amountMoreCups + ";\n" +
-        "int numberOfCustomers = customers.size();\n" +
-        "int numberOfCups = 2;\n" +
-        "int customerServed = 1;\n" +
-        "while(customersServed &lt; numberOfCustomers + 1) {\n" +
-        "    System.out.println(\"Customer #\" + customerServed + \" ordered \" + numberOfCups + \" cups of coffee.\");\n" +
-        "    numberOfCups = numberOfCups + amountMoreCups;\n" +
-        "    customerServed = customerServed + 1;\n" +
-        "}";
+    if (amountMoreCups != null) {
+        document.getElementById("lesson4code0").innerHTML = amountMoreCups;
+        //            "int amountMoreCups = " + amountMoreCups + ";\n" +
+        //            "int numberOfCustomers = customers.size();\n" +
+        //            "int numberOfCups = 2;\n" +
+        //            "int customerServed = 1;\n" +
+        //            "while(customersServed &lt; numberOfCustomers + 1) {\n" +
+        //            "    System.out.println(\"Customer #\" + customerServed + \" ordered \" + numberOfCups + \" cups of coffee.\");\n" +
+        //            "    numberOfCups = numberOfCups + amountMoreCups;\n" +
+        //            "    customerServed = customerServed + 1;\n" +
+        //            "}";
+    }
 }
